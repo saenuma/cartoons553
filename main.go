52948f30aed8255fc0902dde377bdcf253c2da89
Bankole Ojo <ojobankole@gmail.com>
2025-02-19 02:26:48 +0100

    began reorganization of cartoons553
    prep and del command working fine.

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"context"
 	"fmt"
-	"io"
-	"mime/multipart"
-	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/gookit/color"
 	"github.com/saenuma/zazabul"
-	compute "google.golang.org/api/compute/v1"
-	"google.golang.org/api/option"
 )
 
 const VersionFormat = "20060102T150405MST"
@@ -42,19 +35,21 @@ Note: If you quit the program prematurely, go to https://console.cloud.google.co
 the created VM.
 
 Working Directory: '%s'
+All files must be placed in Working Directory (%s)
 
 Supported Commands:
 
-    init    Creates a config file describing your render server. Edit to your own requirements.
-            Some of the values can be gotten from Google Cloud's documentation.
+    prep    Prepares the render server for cartoons553. It would be already configured and kept in
+            in a suspended state. It prints a serverConfigFile
 
-    r       Renders a project with the config created above. It expects a blender file and a
-            launch file (created from 'init' above)
-            All files must be placed in Working Directory (%s)
+    rnd       Renders a project with the config created above. It expects a blender file and a
+            serverConfigFile (created in prep command above)
+
+    del     Deletes a render server. It expects a serverConfigFile
 
 `, rootPath, rootPath)
 
-	case "init":
+	case "prep":
 		var tmpl = `// project is the Google Cloud Project name
 // It can be created either from the Google Cloud Console or from the gcloud command
 project:
@@ -74,7 +69,7 @@ zone:
 // It is not necessary it must be an e2 instance.
 // If you find a render to be slow, use a bigger machine. Preferably a highcpu machine.
 // At times you might need to apply for quota increase to use a bigger machine.
-machine_type: e2-highcpu-16
+machine_type: e2-highcpu-4
 
 
 // sak means service account key file.
@@ -83,33 +78,38 @@ machine_type: e2-highcpu-16
 // It must be placed in the path where this config is.
 sak_file:
 
-// the quality metric here specifies the render engine to use.
-// if the quality is high it would use CYCLES render engine.
-// if the quality is low it would use the EEVEE render engine.
-quality: low
-
-
 	`
 		configFileName := "s" + time.Now().Format(VersionFormat) + ".zconf"
-		writePath := filepath.Join(rootPath, configFileName)
+		confPath := filepath.Join(rootPath, configFileName)
 
 		conf, err := zazabul.ParseConfig(tmpl)
 		if err != nil {
 			panic(err)
 		}
 
-		err = conf.Write(writePath)
+		err = conf.Write(confPath)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("Edit the file at '%s' before launching.\n", writePath)
-
-	case "r":
-		if len(os.Args) != 4 {
-			color.Red.Println("The r command expects a blender file and the conf file")
+		cmd := exec.Command("nano", confPath)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			color.Red.Println(err.Error())
 			os.Exit(1)
 		}
+
+		doPrep(confPath)
+
+	case "rnd":
+		if len(os.Args) != 4 {
+			color.Red.Println("The rnd command expects a blender file and a serverConfigFile")
+			os.Exit(1)
+		}
+
 		blenderPath := filepath.Join(rootPath, os.Args[2])
 		if !DoesPathExists(blenderPath) {
 			color.Red.Printf("The file '%s' does not exist in '%s'", os.Args[2], rootPath)
@@ -117,268 +117,19 @@ quality: low
 		}
 
 		serverConfigPath := filepath.Join(rootPath, os.Args[3])
+		doRender(blenderPath, serverConfigPath)
 
-		conf, err := zazabul.LoadConfigFile(serverConfigPath)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, item := range conf.Items {
-			if item.Value == "" {
-				color.Red.Println("Every field in the launch file is compulsory.")
-				os.Exit(1)
-			}
-		}
-
-		credentialsFilePath := filepath.Join(rootPath, conf.Get("sak_file"))
-		if !DoesPathExists(credentialsFilePath) {
-			color.Red.Printf("The file '%s' does not exist in '%s'\n", conf.Get("sak_file"), rootPath)
+	case "del":
+		if len(os.Args) != 3 {
+			color.Red.Println("The del command expects a serverConfigFile")
 			os.Exit(1)
 		}
 
-		instanceName := fmt.Sprintf("c553-%s", strings.ToLower(UntestedRandomString(4)))
-
-		var startupScript = `
-  	#! /bin/bash
-
-sudo apt update
-sudo apt upgrade
-sudo apt install -y libx11-dev libxxf86vm-dev libxcursor-dev libxi-dev libxrandr-dev libxinerama-dev libegl-dev
-sudo apt install -y libwayland-dev wayland-protocols libxkbcommon-dev libdbus-1-dev linux-libc-dev
-sudo apt install -y libsm6
-sudo snap install blender --classic
-
-sudo mkdir -p /tmp/c553_in/
-sudo rm -rf /tmp/t1/ # clean the output folder incase of reuse.
-
-gcloud compute firewall-rules create c553rules --direction ingress \
- --source-ranges 0.0.0.0/0 --rules tcp:8089 --action allow
-
-# download needed files
-wget https://sae.ng/static/c553/c553_mover
-wget https://sae.ng/static/c553/c553_mover.service
-wget https://sae.ng/static/c553/c553_shutdown
-wget https://sae.ng/static/c553/c553_shutdown.service
-wget https://sae.ng/static/c553/c553_render
-wget https://sae.ng/static/c553/c553_render.service
-
-# put the files in place
-sudo mkdir -p /opt/cartoons553/
-sudo cp c553_mover /opt/cartoons553/c553_mover
-sudo cp c553_shutdown /opt/cartoons553/c553_shutdown
-sudo cp c553_render /opt/cartoons553/c553_render
-sudo chmod +x /opt/cartoons553/c553_mover
-sudo chmod +x /opt/cartoons553/c553_shutdown
-sudo chmod +x /opt/cartoons553/c553_render
-sudo cp c553_mover.service /etc/systemd/system/c553_mover.service
-sudo cp c553_shutdown.service /etc/systemd/system/c553_shutdown.service
-sudo cp c553_render.service /etc/systemd/system/c553_render.service
-
-# start the programs
-sudo systemctl daemon-reload
-sudo systemctl start c553_shutdown
-sudo systemctl start c553_render
-sudo systemctl start c553_mover
-`
-
-		ctx := context.Background()
-		computeService, err := compute.NewService(ctx, option.WithCredentialsFile(credentialsFilePath),
-			option.WithScopes(compute.ComputeScope))
-		if err != nil {
-			panic(err)
-		}
-		prefix := "https://www.googleapis.com/compute/v1/projects/" + conf.Get("project")
-
-		image, err := computeService.Images.GetFromFamily("ubuntu-os-cloud", "ubuntu-minimal-2204-lts").Context(ctx).Do()
-		if err != nil {
-			panic(err)
-		}
-		imageURL := image.SelfLink
-
-		instance := &compute.Instance{
-			Name:        instanceName,
-			Description: "ooldim instance",
-			MachineType: prefix + "/zones/" + conf.Get("zone") + "/machineTypes/" + conf.Get("machine_type"),
-			Disks: []*compute.AttachedDisk{
-				{
-					AutoDelete: true,
-					Boot:       true,
-					Type:       "PERSISTENT",
-
-					InitializeParams: &compute.AttachedDiskInitializeParams{
-						SourceImage: imageURL,
-						DiskType:    prefix + "/zones/" + conf.Get("zone") + "/diskTypes/pd-ssd",
-						DiskSizeGb:  10,
-					},
-				},
-			},
-			NetworkInterfaces: []*compute.NetworkInterface{
-				{
-					AccessConfigs: []*compute.AccessConfig{
-						{
-							Type: "ONE_TO_ONE_NAT",
-							Name: "External NAT",
-						},
-					},
-					Network: prefix + "/global/networks/default",
-				},
-			},
-			ServiceAccounts: []*compute.ServiceAccount{
-				{
-					Email: "default",
-					Scopes: []string{
-						compute.DevstorageFullControlScope,
-						compute.ComputeScope,
-					},
-				},
-			},
-			Metadata: &compute.Metadata{
-				Items: []*compute.MetadataItems{
-					{
-						Key:   "startup-script",
-						Value: &startupScript,
-					},
-				},
-			},
-		}
-
-		op, err := computeService.Instances.Insert(conf.Get("project"), conf.Get("zone"), instance).Do()
-		if err != nil {
-			panic(err)
-		}
-		err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
-		if err != nil {
-			panic(err)
-		}
-
-		launchedInstance, err := computeService.Instances.Get(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
-		if err != nil {
-			panic(err)
-		}
-		instanceIP := launchedInstance.NetworkInterfaces[0].AccessConfigs[0].NatIP
-
-		fmt.Printf("Render Server Created. Name: %s, IP: %s\n", instanceName, instanceIP)
-
-		for {
-			_, err := http.Get("http://" + instanceIP + ":8089/ready")
-			if err != nil {
-				time.Sleep(10 * time.Second)
-				continue
-			}
-			break
-		}
-
-		fmt.Println("Tested connection to your Render server.")
-
-		// write the quality of the render
-		resp, err := http.Get("http://" + instanceIP + ":8089/set_quality/?q=" + conf.Get("quality"))
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer resp.Body.Close()
-		body2, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if resp.StatusCode != 200 {
-			fmt.Println(string(body2))
-		}
-
-		// upload blender file to render
-		rawBlenderFile, err := os.ReadFile(blenderPath)
-		if err != nil {
-			panic(err)
-		}
-
-		body := new(bytes.Buffer)
-		writer := multipart.NewWriter(body)
-		part, err := writer.CreateFormFile("file", filepath.Base(blenderPath))
-		if err != nil {
-			panic(err)
-		}
-		part.Write(rawBlenderFile)
-		err = writer.Close()
-		if err != nil {
-			panic(err)
-		}
-		req, err := http.NewRequest("POST", "http://"+instanceIP+":8089/upload", body)
-		if err != nil {
-			panic(err)
-		}
-		req.Header.Set("Content-Type", writer.FormDataContentType())
-
-		httpClient := &http.Client{}
-		_, err = httpClient.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Uploaded blend file and beginning render")
-
-		startTime := time.Now()
-		fmt.Println()
-		fmt.Printf("Click %s to download a preview of your render while it renders.\n",
-			"http://"+instanceIP+":8089/dlv/")
-
-		for {
-			err := downloadFile("http://"+instanceIP+":8089/dl/?p="+"/tmp/c553_in/done.txt",
-				filepath.Join(rootPath, "done.txt"))
-			if err == nil {
-				break
-			}
-
-			time.Sleep(10 * time.Second)
-			fmt.Printf("\rBeen rendering for: %s  ", time.Since(startTime).Round(time.Second).String())
-		}
-
-		fmt.Println("\nRendered now dowloading.")
-		os.RemoveAll(filepath.Join(rootPath, "done.txt"))
-
-		rootPath, _ := GetRootPath()
-		dlPath := filepath.Join(rootPath, time.Now().Format(VersionFormat)+".avi")
-		err = downloadFile("http://"+instanceIP+":8089/dlv/", dlPath)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("Output: %s\n", dlPath)
-
-		// delete the server
-		op, err = computeService.Instances.Delete(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
-		if err != nil {
-			panic(err)
-		}
-		err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("All Done. Server Deleted.")
+		serverConfigPath := filepath.Join(rootPath, os.Args[2])
+		doDelete(serverConfigPath)
 
 	default:
 		color.Red.Println("Unexpected command. Run the cli with --help to find out the supported commands.")
 		os.Exit(1)
 	}
-}
-
-func waitForOperationZone(project, zone string, service *compute.Service, op *compute.Operation) error {
-	ctx := context.Background()
-	for {
-		result, err := service.ZoneOperations.Get(project, zone, op.Name).Context(ctx).Do()
-		if err != nil {
-			return fmt.Errorf("failed retriving operation status: %s", err)
-		}
-
-		if result.Status == "DONE" {
-			if result.Error != nil {
-				var errors []string
-				for _, e := range result.Error.Errors {
-					errors = append(errors, e.Message)
-				}
-				return fmt.Errorf("operation failed with error(s): %s", strings.Join(errors, ", "))
-			}
-			break
-		}
-		time.Sleep(time.Second)
-	}
-	return nil
 }
---
+++ reactors.go
@@ -0,0 +1,376 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/gookit/color"
+	"github.com/saenuma/zazabul"
+	compute "google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+)
+
+func waitForOperationZone(project, zone string, service *compute.Service, op *compute.Operation) error {
+	ctx := context.Background()
+	for {
+		result, err := service.ZoneOperations.Get(project, zone, op.Name).Context(ctx).Do()
+		if err != nil {
+			return fmt.Errorf("failed retriving operation status: %s", err)
+		}
+
+		if result.Status == "DONE" {
+			if result.Error != nil {
+				var errors []string
+				for _, e := range result.Error.Errors {
+					errors = append(errors, e.Message)
+				}
+				return fmt.Errorf("operation failed with error(s): %s", strings.Join(errors, ", "))
+			}
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	return nil
+}
+
+func doPrep(serverConfigPath string) {
+	rootPath, _ := GetRootPath()
+
+	conf, err := zazabul.LoadConfigFile(serverConfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range conf.Items {
+		if item.Value == "" {
+			color.Red.Println("Every field in the launch file is compulsory.")
+			os.Exit(1)
+		}
+	}
+
+	credentialsFilePath := filepath.Join(rootPath, conf.Get("sak_file"))
+	if !DoesPathExists(credentialsFilePath) {
+		color.Red.Printf("The file '%s' does not exist in '%s'\n", conf.Get("sak_file"), rootPath)
+		os.Exit(1)
+	}
+
+	instanceName := fmt.Sprintf("c553-%s", strings.ToLower(UntestedRandomString(10)))
+
+	var startupScript = `
+	#! /bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y libx11-dev libxxf86vm-dev libxcursor-dev libxi-dev libxrandr-dev libxinerama-dev libegl-dev
+sudo apt install -y libwayland-dev wayland-protocols libxkbcommon-dev libdbus-1-dev linux-libc-dev
+sudo apt install -y libsm6
+sudo snap install blender --classic
+
+sudo mkdir -p /tmp/c553_in/
+sudo rm -rf /tmp/t1/ # clean the output folder incase of reuse.
+
+gcloud compute firewall-rules create c553rules --direction ingress \
+--source-ranges 0.0.0.0/0 --rules tcp:8089 --action allow
+
+# download needed files
+wget https://sae.ng/static/c553/c553_mover
+wget https://sae.ng/static/c553/c553_mover.service
+wget https://sae.ng/static/c553/c553_shutdown
+wget https://sae.ng/static/c553/c553_shutdown.service
+wget https://sae.ng/static/c553/c553_render
+wget https://sae.ng/static/c553/c553_render.service
+
+# put the files in place
+sudo mkdir -p /opt/cartoons553/
+sudo cp c553_mover /opt/cartoons553/c553_mover
+sudo cp c553_shutdown /opt/cartoons553/c553_shutdown
+sudo cp c553_render /opt/cartoons553/c553_render
+sudo chmod +x /opt/cartoons553/c553_mover
+sudo chmod +x /opt/cartoons553/c553_shutdown
+sudo chmod +x /opt/cartoons553/c553_render
+sudo cp c553_mover.service /etc/systemd/system/c553_mover.service
+sudo cp c553_shutdown.service /etc/systemd/system/c553_shutdown.service
+sudo cp c553_render.service /etc/systemd/system/c553_render.service
+
+# start the programs
+sudo systemctl daemon-reload
+sudo systemctl start c553_shutdown
+sudo systemctl start c553_render
+sudo systemctl start c553_mover
+`
+
+	ctx := context.Background()
+	computeService, err := compute.NewService(ctx, option.WithCredentialsFile(credentialsFilePath),
+		option.WithScopes(compute.ComputeScope))
+	if err != nil {
+		panic(err)
+	}
+	prefix := "https://www.googleapis.com/compute/v1/projects/" + conf.Get("project")
+
+	image, err := computeService.Images.GetFromFamily("ubuntu-os-cloud", "ubuntu-minimal-2204-lts").Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	imageURL := image.SelfLink
+
+	instance := &compute.Instance{
+		Name:        instanceName,
+		Description: "ooldim instance",
+		MachineType: prefix + "/zones/" + conf.Get("zone") + "/machineTypes/" + conf.Get("machine_type"),
+		Disks: []*compute.AttachedDisk{
+			{
+				AutoDelete: true,
+				Boot:       true,
+				Type:       "PERSISTENT",
+
+				InitializeParams: &compute.AttachedDiskInitializeParams{
+					SourceImage: imageURL,
+					DiskType:    prefix + "/zones/" + conf.Get("zone") + "/diskTypes/pd-ssd",
+					DiskSizeGb:  10,
+				},
+			},
+		},
+		NetworkInterfaces: []*compute.NetworkInterface{
+			{
+				AccessConfigs: []*compute.AccessConfig{
+					{
+						Type: "ONE_TO_ONE_NAT",
+						Name: "External NAT",
+					},
+				},
+				Network: prefix + "/global/networks/default",
+			},
+		},
+		ServiceAccounts: []*compute.ServiceAccount{
+			{
+				Email: "default",
+				Scopes: []string{
+					compute.DevstorageFullControlScope,
+					compute.ComputeScope,
+				},
+			},
+		},
+		Metadata: &compute.Metadata{
+			Items: []*compute.MetadataItems{
+				{
+					Key:   "startup-script",
+					Value: &startupScript,
+				},
+			},
+		},
+	}
+
+	op, err := computeService.Instances.Insert(conf.Get("project"), conf.Get("zone"), instance).Do()
+	if err != nil {
+		panic(err)
+	}
+	err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Started render server")
+
+	launchedInstance, err := computeService.Instances.Get(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	instanceIP := launchedInstance.NetworkInterfaces[0].AccessConfigs[0].NatIP
+
+	for {
+		_, err := http.Get("http://" + instanceIP + ":8089/ready")
+		if err != nil {
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		break
+	}
+
+	// stops the server
+	op, err = computeService.Instances.Stop(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Finished configuring render server.")
+	raw, _ := os.ReadFile(serverConfigPath)
+	newRaw := string(raw) + "\n\n" + "name: " + instanceName
+	os.WriteFile(serverConfigPath, []byte(newRaw), 0777)
+	fmt.Println("Server config path: ", serverConfigPath)
+}
+
+func doRender(blenderPath, serverConfigPath string) {
+	rootPath, _ := GetRootPath()
+
+	conf, err := zazabul.LoadConfigFile(serverConfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range conf.Items {
+		if item.Value == "" {
+			color.Red.Println("Every field in the launch file is compulsory.")
+			os.Exit(1)
+		}
+	}
+	credentialsFilePath := filepath.Join(rootPath, conf.Get("sak_file"))
+	if !DoesPathExists(credentialsFilePath) {
+		color.Red.Printf("The file '%s' does not exist in '%s'\n", conf.Get("sak_file"), rootPath)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	computeService, err := compute.NewService(ctx, option.WithCredentialsFile(credentialsFilePath),
+		option.WithScopes(compute.ComputeScope))
+	if err != nil {
+		panic(err)
+	}
+	instanceName := conf.Get("name")
+
+	// starts the server
+	op, err := computeService.Instances.Start(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
+	if err != nil {
+		panic(err)
+	}
+
+	launchedInstance, err := computeService.Instances.Get(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	instanceIP := launchedInstance.NetworkInterfaces[0].AccessConfigs[0].NatIP
+
+	for {
+		_, err := http.Get("http://" + instanceIP + ":8089/ready")
+		if err != nil {
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		break
+	}
+
+	// upload blender file to render
+	rawBlenderFile, err := os.ReadFile(blenderPath)
+	if err != nil {
+		panic(err)
+	}
+
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(blenderPath))
+	if err != nil {
+		panic(err)
+	}
+	part.Write(rawBlenderFile)
+	err = writer.Close()
+	if err != nil {
+		panic(err)
+	}
+	req, err := http.NewRequest("POST", "http://"+instanceIP+":8089/upload", body)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	httpClient := &http.Client{}
+	_, err = httpClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Uploaded blend file and beginning render")
+
+	startTime := time.Now()
+	fmt.Println()
+	fmt.Printf("Click %s to download a preview of your render while it renders.\n",
+		"http://"+instanceIP+":8089/dlv/")
+
+	for {
+		err := downloadFile("http://"+instanceIP+":8089/dl/?p="+"/tmp/c553_in/done.txt",
+			filepath.Join(rootPath, "done.txt"))
+		if err == nil {
+			break
+		}
+
+		time.Sleep(10 * time.Second)
+		fmt.Printf("\rBeen rendering for: %s  ", time.Since(startTime).Round(time.Second).String())
+	}
+
+	fmt.Println("\nRendered now dowloading.")
+	os.RemoveAll(filepath.Join(rootPath, "done.txt"))
+
+	dlPath := filepath.Join(rootPath, time.Now().Format(VersionFormat)+".avi")
+	err = downloadFile("http://"+instanceIP+":8089/dlv/", dlPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Output: %s\n", dlPath)
+
+	// stops the server
+	op, err = computeService.Instances.Stop(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Server stopped.")
+}
+
+func doDelete(serverConfigPath string) {
+	rootPath, _ := GetRootPath()
+
+	conf, err := zazabul.LoadConfigFile(serverConfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range conf.Items {
+		if item.Value == "" {
+			color.Red.Println("Every field in the launch file is compulsory.")
+			os.Exit(1)
+		}
+	}
+	credentialsFilePath := filepath.Join(rootPath, conf.Get("sak_file"))
+	if !DoesPathExists(credentialsFilePath) {
+		color.Red.Printf("The file '%s' does not exist in '%s'\n", conf.Get("sak_file"), rootPath)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	computeService, err := compute.NewService(ctx, option.WithCredentialsFile(credentialsFilePath),
+		option.WithScopes(compute.ComputeScope))
+	if err != nil {
+		panic(err)
+	}
+	instanceName := conf.Get("name")
+
+	// delete the server
+	op, err := computeService.Instances.Delete(conf.Get("project"), conf.Get("zone"), instanceName).Context(ctx).Do()
+	if err != nil {
+		panic(err)
+	}
+	err = waitForOperationZone(conf.Get("project"), conf.Get("zone"), computeService, op)
+	if err != nil {
+		panic(err)
+	}
+
+	os.RemoveAll(serverConfigPath)
+	fmt.Println("All Done. Server Deleted.")
+}
