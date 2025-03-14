7ab677aab55057ee8f40b693923253e1fddd83f8
Bankole Ojo <ojobankole@gmail.com>
2025-03-14 23:31:39 +0100

    updated cartoons553 help file

--- main.go
+++ main.go
@@ -27,12 +27,11 @@ func main() {
 
 	switch os.Args[1] {
 	case "--help", "help", "h":
-		fmt.Printf(`cartoons553 creates a GCP VM, renders a blender project on it, downloads the renders and deletes the VM.
+		fmt.Printf(`cartoons553 helps in rendering a blender project on Google Cloud.
 
 Note: Please try launching your choice server on Google Cloud's website before using it here.
 
-Note: If you quit the program prematurely, go to https://console.cloud.google.com to delete
-the created VM.
+Note: 100 - 200 CPUs is recommended for rendering blender projects.
 
 Working Directory: '%s'
 All files must be placed in Working Directory (%s)
@@ -42,7 +41,7 @@ Supported Commands:
     prep    Prepares the render server for cartoons553. It would be already configured and kept in
             in a suspended state. It prints a serverConfigFile
 
-    rnd       Renders a project with the config created above. It expects a blender file and a
+    rnd     Renders a project with the config created above. It expects a blender file and a
             serverConfigFile (created in prep command above)
 
     del     Deletes a render server. It expects a serverConfigFile
